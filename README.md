39f616adf2d67687095e5863447e44bae2fe63f3
EvanRedVan <106684791+EvanRedVan@users.noreply.github.com>
2023-05-11 22:38:42 -0500

    Add README

---
+++ README.md
@@ -0,0 +1,8 @@
+##### Bill tracking web app built with express.js, mongodb, ejs, jquery and bootstrap.
+
+- A minimalistic full stack bill tracking application built with the stack listed above. Also includes user registration and login which is implemented with passport.js and bcrypt.js.
+
+- Currently setup to store data in a local monogodb database.
+
+![Bill Dashboard](ExampleImages/bill_dashboard.png)
+
--- routes/inputExpense.js
+++ routes/inputExpense.js
@@ -4,17 +4,20 @@ const User = require('../models/User');
 
 router.post('/', async (req, res) => {
 	const user = await User.findById(req.user.id);
+	const newBill = {};
 
-	const newBill = {
-		expenseName: req.body.expenseName,
-		expenseAmount: req.body.expenseAmount,
-		dueDate: req.body.dueDate,
-		draftType: req.body.draftType
+	if (req.body.draftType === "Auto-draft") {
+			newBill.status = "Auto"
 	}
 
-	user.expenses.push(newBill);
-	await user.save();
+	newBill.expenseName = req.body.expenseName
+	newBill.expenseAmount = req.body.expenseAmount
+	newBill.dueDate = req.body.dueDate
+	newBill.draftType = req.body.draftType
 
+	user.expenses.push(newBill);
+
+	await user.save();
 	
 	if (req.user) {
 		res.redirect('/user/finance');
